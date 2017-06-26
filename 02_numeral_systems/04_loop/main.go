2a89443691f82085dc2adcdda42f7adf3f6b90a8
Chris Williams <chriswilliams1977@gmail.com>
2017-06-26 08:27:28 +0200

    go files

---
+++ 02_numeral_systems/04_loop/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	for i := 0; i < 200; i++ {
+	//		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	//	}
+
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+	}
+}
+
+//}
